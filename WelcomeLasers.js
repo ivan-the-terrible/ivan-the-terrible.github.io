1d70f3d453bea18530c4c4f5168a661709bfb164
Ivan Chwalik <ivan.chwalik@callibrity.com>
2023-05-15 15:48:34 -0400

    Determined font size dynamically

--- WelcomeLasers.js
+++ WelcomeLasers.js
@@ -17,24 +17,49 @@ function adjust() {
 function AttachEvents(element, type, handler) {
   element.addEventListener(type, handler);
 }
+function determineFontSize() {
+  //Following Bootstrap's criteria for response breakpoints
+  const IsExtraSmall = screenSize => screenSize < 576;
+  const IsSmall = screenSize => screenSize >= 576 && screenSize < 768;
+  const IsMedium = screenSize => screenSize >= 768 && screenSize < 992;
+  const IsLarge = screenSize => screenSize >= 992 && screenSize < 1200;
+  const IsExtraLarge = screenSize => screenSize >= 1200;
+
+  const screenSize = canvas.width;
+  const baseSize = 30;
+  switch (true) {
+    case IsExtraSmall(screenSize):
+      return baseSize;
+    case IsSmall(screenSize):
+      return baseSize + 15;
+    case IsMedium(screenSize):
+      return baseSize * 2;
+    case IsLarge(screenSize):
+      return baseSize * 2 + 15;
+    case IsExtraLarge(screenSize):
+      return baseSize * 3;
+  }
+}
 function drawDynamicText() {
   const welcomeText = ctx.measureText("Welcome!");
   console.log(welcomeText);
   //Holy Guacamole
   //https://stackoverflow.com/questions/1451635/how-to-make-canvas-text-selectable
   ctx.strokeStyle = "red";
-  let x = canvasWidth - welcomeText.actualBoundingBoxLeft;
-  let y = canvasHeight - welcomeText.actualBoundingBoxAscent;
+  let x = canvas.width / 2 - welcomeText.actualBoundingBoxLeft;
+  let y = canvas.height / 2 - welcomeText.actualBoundingBoxAscent;
   let height = welcomeText.actualBoundingBoxAscent;
   ctx.strokeRect(x, y, welcomeText.width, height);
+
+  const w = {};
+  console.log({ x, y, height });
 }
-
 function welcomeLasers() {
-  const canvasWidth = canvas.width / 2;
-  const canvasHeight = canvas.height / 2;
-
-  ctx.font = "30px Helvetica";
+  const fontSize = determineFontSize();
+  ctx.font = fontSize + "px Helvetica";
   ctx.textAlign = "center";
   ctx.fillStyle = "white";
-  ctx.fillText("Welcome!", canvasWidth, canvasHeight);
+  ctx.fillText("Welcome!", canvas.width / 2, canvas.height / 2);
+
+  drawDynamicText();
 }
