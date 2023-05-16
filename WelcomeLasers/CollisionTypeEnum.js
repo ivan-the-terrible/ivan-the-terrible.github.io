8bd5106016b283f345361a71585d212a9709c2f3
Ivan Chwalik <ivan.chwalik@callibrity.com>
2023-05-16 14:42:46 -0400

    Added CollisionTypes enum

---
+++ WelcomeLasers/CollisionTypeEnum.js
@@ -0,0 +1,12 @@
+const CollisionType = {
+  Canvas: "Canvas",
+  Letter: "Letter",
+  Top: "Top",
+  Bottom: "Bottom",
+  Right: "Right",
+  Left: "Left",
+};
+
+Object.freeze(CollisionType);
+
+export { CollisionType };
