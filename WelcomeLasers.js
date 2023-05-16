fc498dffa1e664ecfe46adc1efd7f34db0c292db
Ivan Chwalik <ivan.chwalik@callibrity.com>
2023-05-15 20:25:18 -0400

    Letter layouts through placeLetter function

--- WelcomeLasers.js
+++ WelcomeLasers.js
@@ -40,16 +40,66 @@ function determineFontSize() {
       return baseSize * 3;
   }
 }
+function placeLetter(length, currentIndex, letterLayout) {
+  const xBase = canvas.width / length;
+  const yBase = canvas.height / length;
+
+  let x;
+  let y;
+  //TODO: Maybe I put in a case that pops the letters up and down from the middle
+  switch (letterLayout) {
+    case 0: //This is orients the letters diagonally
+      x = xBase + xBase * currentIndex;
+      y = yBase + yBase * currentIndex;
+      //Keep it off the border
+      if (currentIndex == 0) {
+        y += 25;
+      } else if (currentIndex == length - 1) {
+        x -= 35;
+      }
+      break;
+    case 1: //This orients the letters from bottom left to top right (reverse diagonal)
+      x = xBase + xBase * currentIndex;
+      y = yBase + yBase * (length - currentIndex);
+      //Keep it off the border
+      if (currentIndex == 0) {
+        y -= 55;
+      } else if (currentIndex == length - 1) {
+        x -= 35;
+        y -= 35;
+      } else {
+        y -= 55;
+      }
+      break;
+    default: //This orients the letters normally, from left to right
+      x = xBase + xBase * currentIndex;
+      y = canvas.height / 2;
+      if (currentIndex == length - 1) {
+        x -= 35;
+      }
+      break;
+  }
+  return { x, y };
+}
 function drawDynamicText() {
-  const welcomeText = ctx.measureText("Welcome!");
-  console.log(welcomeText);
+  const message = "Welcome!";
+  const letterLayout = Math.floor(Math.random() * 3);
+  for (let i = 0; i < message.length; i++) {
+    const character = message[i];
+    const textMeasurements = ctx.measureText(character);
+    const letter = placeLetter(message.length, i, letterLayout);
+    ctx.fillText(character, letter.x, letter.y);
+  }
+
+  const textMeasurements = ctx.measureText("Welcome!");
+  console.log(textMeasurements);
   //Holy Guacamole
   //https://stackoverflow.com/questions/1451635/how-to-make-canvas-text-selectable
   ctx.strokeStyle = "red";
-  let x = canvas.width / 2 - welcomeText.actualBoundingBoxLeft;
-  let y = canvas.height / 2 - welcomeText.actualBoundingBoxAscent;
-  let height = welcomeText.actualBoundingBoxAscent;
-  ctx.strokeRect(x, y, welcomeText.width, height);
+  let x = canvas.width / 2 - textMeasurements.actualBoundingBoxLeft;
+  let y = canvas.height / 2 - textMeasurements.actualBoundingBoxAscent;
+  let height = textMeasurements.actualBoundingBoxAscent;
+  //ctx.strokeRect(x, y, textMeasurements.width, height);
 
   const w = {};
   console.log({ x, y, height });
@@ -59,7 +109,5 @@ function welcomeLasers() {
   ctx.font = fontSize + "px Helvetica";
   ctx.textAlign = "center";
   ctx.fillStyle = "white";
-  ctx.fillText("Welcome!", canvas.width / 2, canvas.height / 2);
-
   drawDynamicText();
 }
